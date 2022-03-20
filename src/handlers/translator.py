8f56a286a74605b58fe8164b457b8074803d35cb
crot <todd.develop@gmail.com>
2022-03-20 16:32:49 -0400

    .

---
+++ src/handlers/translator.py
@@ -0,0 +1,12 @@
+from unittest import main
+from googletrans import Translator
+
+
+def translate():
+    translator = Translator(service_urls=[
+      'translate.google.com',
+      'translate.google.co.kr',
+    ])
+
+    print(translator.detect('hello'))
+
