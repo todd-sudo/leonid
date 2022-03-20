848236c5ea16c2908493645bde272576fb6bd870
todd <todd.develop@gmail.com>
2022-03-20 21:17:55 +0300

    upd

--- src/handlers/tesseract.py
+++ src/handlers/tesseract.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 def image_to_text(image: str, lang: str):
     img = Image.open(image)
-    custom_config = r'--oem --psm 13 --tessdata-dir "/usr/share/tesseract-ocr/4.00/tessdata"'
+    custom_config = r'--oem --psm 13 --tessdata-dir "/usr/share/tesseract-ocr/4.00/tessdata/"'
     text = pytesseract.image_to_string(
         image=img, config=custom_config, lang=lang
     ).strip()
